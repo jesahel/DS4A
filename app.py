ecade61d25a71db6fc9a976d4c0479ba1ed37abc
Ubuntu <ubuntu@ip-172-31-43-150.us-east-2.compute.internal>
2019-11-08 02:55:12 +0000

    Added app.py with host rule file

--- app.py
+++ app.py
@@ -342,4 +342,4 @@ def update_balance_plot(exchange, leverage, start_date, end_date):
                         }
                         
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run_server(debug=True,host= '0.0.0.0')
